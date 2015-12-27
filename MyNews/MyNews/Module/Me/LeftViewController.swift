e78221fb0c364f23be4d9f31f3584f4007432d6b
hetengfei <hetengfei163@126.com>
2015-12-27 22:33:02 +0800

    引入框架 添加heardImageView视图为抽屉

--- MyNews/MyNews/Module/Me/LeftViewController.swift
+++ MyNews/MyNews/Module/Me/LeftViewController.swift
@@ -12,29 +12,53 @@ class LeftViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        setupUI()
     }
 
     private func setupUI(){
     
         view.addSubview(personalView)
-        
-    
-    }
-    
-    private func setPersonalView(){
-    
         personalView.addSubview(heardImageView)
+        personalView.addSubview(nameBtn)
+        
+        personalView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(view.snp_top).offset(80)
+            make.height.equalTo(100)
+            make.width.equalTo(200)
+            make.left.equalTo(view.snp_left)
+        }
+        heardImageView.snp_makeConstraints { (make) -> Void in
+            make.left.equalTo(personalView.snp_left).offset(10)
+            make.top.equalTo(personalView.snp_top).offset(10)
+            make.width.height.equalTo(60)
+        }
+        nameBtn.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(heardImageView.snp_top)
+            make.left.equalTo(heardImageView.snp_right).offset(10)
+        }
         
     
     }
     
     //MARK:- 懒加载视图
     private lazy var tableView: UITableView = UITableView()
-    private lazy var personalView: UIView = UIView()
+    private lazy var personalView: UIView = {
+        
+        let personalView = UIView()
+        personalView.backgroundColor = UIColor.whiteColor()
+        return personalView
+        
+    }()
     private lazy var heardImageView : UIImageView = UIImageView(image: UIImage(named: "pa"))
     
+    private lazy var nameBtn: UIButton = {
+        
+        let btn = UIButton()
+        btn.setTitle("MyNews", forState: .Normal)
+        btn.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        return btn
+    
+    }()
     
 
 }
